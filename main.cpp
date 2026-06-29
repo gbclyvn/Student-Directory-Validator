2293278c51647359fab66cb35df51f38f80c589e
gabrielclydecclavano@gmail.com <gabrielclydeccclavano@gmail.com>
2026-06-29 21:15:18 +0800

    initial commit

---
+++ main.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <iomanip> 
+using namespace std;
+
+int main() {
+    string studentsID;
+    float lab1, lab2, lab3, average;
+
+    cout << "Enter Students ID: ";
+    cin >> studentsID;
+
+    cout << "Enter Lab 1 Score: ";
+    cin >> lab1;
+
+    cout << "Enter Lab 2 score: ";
+    cin >> lab2;
+
+    cout << "Enter Lab 3 score: ";
+    cin >> lab3;
+
+    average = (lab1 + lab2 + lab3) / 3;
+
+    cout << endl;
+    cout<< "StudentsID:   " << studentsID << endl;
+    cout << "Average Score: " << fixed <<
+    setprecision(4) << average << endl;
+
+    return 0;
+}
